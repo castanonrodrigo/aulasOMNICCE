2b5dfdfd3c307e3d936157a7f8a2f8843f45b0f5
Rodrigo Castanon de Carvalho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2020-05-02 17:37:51 -0300

    final da aula 6(introduzindo useState)

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [state, setState] = useState("nosso primeiro state!");
+  console.log("nosso state:", state);
   let count= 0; 
   function somador(){
     count++;
@@ -10,7 +12,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>{count}</Text>
-      <Button title="aumentar" onPress={()=> somador()}/>
+      <Button title="aumentar" onPress={somador}/>
     </View>
   );
 }
