9b74efed27f80513d1054df869be6dd71f905156
Rodrigo Castanon de Carvalho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2020-05-03 16:07:24 -0300

    final da aula 18

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LoginScreen from './screens/LoginScreen'
-import Exemplo from './screens/Exemplo'
+
 
 const Stack = createStackNavigator();
 
-export default function Routes(){
+export default function(){
     return(
         <NavigationContainer>
             <Stack.Navigator headerMode="none">
                 <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Exemplo" component={Exemplo} />
             </Stack.Navigator>
         </NavigationContainer>
 
