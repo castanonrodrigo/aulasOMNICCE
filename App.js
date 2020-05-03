0fe41686f0a02138cc22d075040c3bf61ae27543
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2020-05-02 22:29:11 -0300

    Final da Aula 11-Imagem do titulo

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { SafeAreaView, View, StyleSheet } from "react-native";
+import { SafeAreaView, View, StyleSheet, Image } from "react-native";
 
 import * as screen from "./src/constants/dimensions";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}></View>
+      <View style={styles.header}>
+        <Image
+          source={require("./src/assets/images/covidTitle.png")}
+          style={styles.titleImage}
+          resizeMode="contain"
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -18,7 +24,14 @@ const styles = StyleSheet.create({
   },
   header: {
     height: screen.height * 0.3,
-    width: screen.width * 0.3,
+    width: screen.width * 1,
     backgroundColor: "red",
+    alignItems: "flex-end",
+    marginTop: screen.height * 0.1,
+  },
+  titleImage: {
+    width: screen.width * 0.8,
+    height: screen.height * 0.2,
+    marginRight: screen.width * 0.04,
   },
 });
