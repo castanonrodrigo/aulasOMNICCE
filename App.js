4a1eea6ecf3e0961e183c9f0acb3a3dc8fda742c
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2020-05-02 21:24:40 -0300

    Final da Aula 9: Reestruturando código

--- App.js
+++ App.js
@@ -1,32 +1,22 @@
 import React from "react";
 import { SafeAreaView, View, StyleSheet } from "react-native";
-import { screenWidth, screenHeight } from "./src/constants/dimensions";
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.background}>
-      <View style={styles.header}>
-        <View style={styles.title}></View>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}></View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  background: {
+  container: {
     flex: 1,
-    backgroundColor: "red",
-    paddingTop: screenHeight * 0.04,
+    backgroundColor: "#FFFFFF",
   },
   header: {
-    height: screenHeight * 0.3,
-    flexDirection: "row-reverse",
-    backgroundColor: "blue",
-    marginVertical: screenHeight * 0.03,
-  },
-  title: {
-    width: screenWidth * 0.8,
-    height: screenHeight * 0.2,
-    backgroundColor: "yellow",
+    height: 200,
+    width: 100,
+    backgroundColor: "red",
   },
 });
