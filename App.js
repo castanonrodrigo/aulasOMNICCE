c74aace367fc763a1bb2abb0e98eb7acb0817833
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2020-05-02 22:49:23 -0300

    Final da Aula 12-Imagegif

--- App.js
+++ App.js
@@ -13,6 +13,13 @@ export default function App() {
           resizeMode="contain"
         />
       </View>
+      <View style={styles.gifContainer}>
+        <Image
+          source={require("./src/assets/images/corona-doctor.gif")}
+          style={styles.gifImage}
+          resizeMode="cover"
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -23,15 +30,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
   },
   header: {
-    height: screen.height * 0.3,
+    // height: screen.height * 0.3,
     width: screen.width * 1,
-    backgroundColor: "red",
+    // backgroundColor: "red",
     alignItems: "flex-end",
-    marginTop: screen.height * 0.1,
+    marginTop: screen.height * 0.08,
   },
   titleImage: {
     width: screen.width * 0.8,
     height: screen.height * 0.2,
     marginRight: screen.width * 0.04,
+  },
+  gifContainer: {
+    // height: 200,
+  },
+  gifImage: {
+    width: screen.width * 1,
+    height: screen.height * 0.45,
   },
 });
