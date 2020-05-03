83c1d7404b075ffb7f97403a207fc7a70592bd7b
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2020-05-02 21:43:54 -0300

    Final da Aula 10-Padronizando Dimensoes

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { SafeAreaView, View, StyleSheet } from "react-native";
 
+import * as screen from "./src/constants/dimensions";
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
@@ -15,8 +17,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
   },
   header: {
-    height: 200,
-    width: 100,
+    height: screen.height * 0.3,
+    width: screen.width * 0.3,
     backgroundColor: "red",
   },
 });
