12b744dc2dbaa059fad2d447fb65a237efd64f53
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2020-05-04 02:15:29 -0300

    Final da Aula 29: Header

--- src/screens/StatsScreen.js
+++ src/screens/StatsScreen.js
@@ -1,10 +1,31 @@
 import React from "react";
-import { View } from "react-native";
+import { SafeAreaView, View, StyleSheet, Image } from "react-native";
+import * as screen from "../constants/dimensions";
 
-// import { Container } from './styles';
+export default function Stats() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("../assets/images/covidTitle_small.png")}
+          style={styles.headerTitle}
+          resizeMode={"contain"}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
 
-const StatsScreen = () => {
-  return <View />;
-};
-
-export default StatsScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    marginTop: screen.height * 0.06,
+    paddingHorizontal: screen.width * 0.04,
+  },
+  headerTitle: {
+    width: screen.width * 0.8,
+    height: screen.height * 0.1,
+  },
+});
