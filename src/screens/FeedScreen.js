fb57c2d6aad04156c78d6183f0eb7f37dd606124
Rodrigo Castanon de Carvalho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2020-05-05 16:44:06 -0300

    final da aula 45: incio da flatlist no feed

--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -1,12 +1,20 @@
-import React, { useEffect } from "react";
-import { View, Button, AsyncStorage } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  Button,
+  AsyncStorage,
+  FlatList,
+  Text,
+} from "react-native";
 import api from "../services/api";
 
 export default function FeedScreen({ navigation: { navigate } }) {
+  const [posts, setPosts] = useState(null);
+
   async function loadPosts() {
     try {
       const response = await api.get("/postagens/");
-      console.log("nossa data:", response.data);
+      setPosts(response.data);
     } catch (e) {
       console.log(e);
     }
@@ -25,10 +33,15 @@ export default function FeedScreen({ navigation: { navigate } }) {
     }
   }
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <SafeAreaView
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+    >
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => <Text>{item.usuario}</Text>}
+      />
       <Button title="LOGOUT" onPress={() => Logout()} />
-      <Button title="fazer um comentário" onPress={() => navigate("comment")} />
-      <Button title="fazer um post" onPress={() => navigate("posting")} />
-    </View>
+    </SafeAreaView>
   );
 }
