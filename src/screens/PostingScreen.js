9b238bc5cbf76c2111c8dbfed1d6975cbf5c127f
Rodrigo Castanon de Carvalho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2020-05-05 23:40:47 -0300

    final da aula 55: state na tela de posting

--- src/screens/PostingScreen.js
+++ src/screens/PostingScreen.js
@@ -1,21 +1,37 @@
-import React from "react";
-import { SafeAreaView, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import { height, width } from "../constants/dimensions";
 
 // import { Container } from './styles';
 
 const PostingScreen = () => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.titleInput}
-        placeholder="Título da sua publicação"
-      />
-      <TextInput
-        style={styles.textInput}
-        placeholder="O que está acontecendo na sua quarentena?"
-      />
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          value={title}
+          onChangeText={(text) => setTitle(text)}
+          style={styles.titleInput}
+          placeholder="Título da sua publicação"
+        />
+        <TextInput
+          value={text}
+          onChangeText={(text) => setText(text)}
+          style={styles.textInput}
+          placeholder="O que está acontecendo na sua quarentena?"
+          multiline
+        />
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -25,19 +41,27 @@ const styles = StyleSheet.create({
   },
   titleInput: {
     height: height * 0.07,
-    borderColor: "black",
-    borderWidth: 5,
+    borderColor: "#39cb7f",
+    backgroundColor: "#dadada",
+    borderWidth: 2,
+    borderRadius: 10,
     width: width * 0.9,
     alignSelf: "center",
     marginTop: 10,
+    fontSize: 20,
+    padding: 5,
   },
   textInput: {
-    borderColor: "black",
-    borderWidth: 5,
+    borderColor: "#39cb7f",
+    backgroundColor: "#dadada",
+    borderWidth: 2,
+    borderRadius: 10,
     height: height * 0.3,
     marginTop: height * 0.01,
     width: width * 0.9,
     alignSelf: "center",
+    fontSize: 20,
+    padding: 5,
   },
 });
 
