240ffe4c6fe3b774e880fd46bbc5e00cb2327b9e
Rodrigo Castanon de Carvalho <rodrigo@MacBook-Pro-de-Rodrigo.local>
2020-05-05 22:31:49 -0300

    final da aula 51: trabalhando no header do feed

--- src/components/FeedHeader.js
+++ src/components/FeedHeader.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, Text } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { height, width } from "../constants/dimensions";
 // import { Container } from './styles';
 
 const FeedHeader = () => {
@@ -10,6 +12,11 @@ const FeedHeader = () => {
         style={styles.image}
         resizeMode="contain"
       />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.text}>
+          O que está acontecendo na sua quarentena?
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -19,8 +26,21 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
-    width: 200,
-    height: 100,
+    width: width * 0.55,
+    height: height * 0.05,
+  },
+  button: {
+    borderWidth: 2,
+    borderRadius: 15,
+    borderColor: "#39cb7f",
+    width: width * 0.9,
+    height: height * 0.1,
+    justifyContent: "center",
+  },
+  text: {
+    color: "gray",
+    fontSize: 22,
+    textAlign: "center",
   },
 });
 
